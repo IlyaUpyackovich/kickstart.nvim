247e713c49554d17dd9e76f94e7130873432cd51
IlyaUpyackovich <zixelant@gmail.com>
2025-05-15 17:27:45 +0300

    feat: add hotkey for new tab

--- init.lua
+++ init.lua
@@ -208,6 +208,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>')
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
